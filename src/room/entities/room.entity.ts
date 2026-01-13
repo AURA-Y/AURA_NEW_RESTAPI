d0c1e0bb81a61f69993bfa75422f294645091cc7
신지웅 <gombang8047@gmail.com>
2026-01-14 04:09:23 +0900

    [신지웅] 수정

--- src/room/entities/room.entity.ts
+++ src/room/entities/room.entity.ts
@@ -23,10 +23,10 @@ export class Room {
   @Column({ type: "varchar", length: 50, nullable: true })
   roomPassword: string | null;
 
-  @Column({ type: "varchar", length: 255, unique: true })
+  @Column({ type: "varchar", length: 255, unique: true, default: () => "gen_random_uuid()" })
   roomShareLink: string;
 
-  @Column({ type: "timestamp with time zone", default: () => "NOW()" })
+  @Column({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
   createdAt: Date;
 
   @Column({ type: "uuid", nullable: false })
