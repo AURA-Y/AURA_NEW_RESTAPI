91947e1abba2e8712bd43e614587092cce3ba106
dongggggggg <dlehdrb020@gmail.com>
2026-01-14 02:24:09 +0900

    [이동규] feat: 구글 캘린더 기능 추가

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [CalendarController],
   providers: [CalendarService],
   exports: [CalendarService],
